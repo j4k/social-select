02c7ead90c689ec5203d3c32a54a46fc8986151e
Jack Wall <jw@jack.gd>
2014-12-10 21:36:56 +0000

    amd/jquery factory pattern

--- src/social.js
+++ src/social.js
@@ -1,54 +1,64 @@
+(function (factory) {
+    if (typeof define === 'function' && define.amd) { 
+        // AMD
+        define(['jquery', 'underscore'], factory);    
+    } else {
+        // No AMD.
+        factory(jQuery, _);
+    }
+}
+}(function ($, _) {
+   
+    // Defaults
+    var defaults = {
+        validAncestors: ['body'],
+        twitterTpl: "https://twitter.com/intent/tweet?text="{{text}}"&url={{url}}",
+        twitterMessageLimit: 140,
+        emailHrefTemplate: "mailto:?subject={{subject}}&body="{{selection}}" {{url}}'
+    };
 
-;(function ( $, window, document, undefined ) {
- 
-    // Create the defaults once
-    var pluginName = "socialSelect",
-        defaults = {
-            validAncestors: ['body'],
-            twitterTpl: "https://twitter.com/intent/tweet?text="{{text}}"&url={{url}}",
-            twitterMessageLimit: 140,
-            emailHrefTemplate: "mailto:?subject={{subject}}&body="{{selection}}" {{url}}'
+    // Set up vars
+    var $body = $(document.body),
+        $twitterEl,
+        $emailEl;
+
+    $.fn.socialSelect = function(){
+        this.init = function(){
+            if(!this.hasTouchScreen()) {
+                $body.append($selectionSharing);
+                $twitterEl = $('');
+                $emailEl = $('');
+                // set binds
+                $('body').on('keypress keydown', _.debounce(this.updateSelection, 50)); 
+                $('body').on('mouseup', _.debounce(this.updateSelection, 200));
+                $('body').on('mousedown', _.debounce(this.updateSelection, 50));
+            }
+        };
+        
+        this.updateSelection = function(){
+           var selection = window.getSelection && document.createRange && window.getSelection(),
+               range,
+               twitterMessage,
+               twitterHref,
+               emailHref;
+
+           if( selection && selection.rangeCount > 0 && selection.toString() ){
+               range = selection.getRangeAt(0);
+               twitterMessage = range.toString();
+
+               if(!$.fn.socialSelect.isValidSelection(range) ){
+                // hideSelection()
+                    return;
+               }
+
+           }
         };
 
-    // Set up variables
-    var $body = $(document.body),
-        $twitterAction,
-        $emailAction;
-    
- 
-    // The actual plugin constructor
-    function Plugin( element, options ) {
-        this.element = element;
- 
-        this.options = $.extend( {}, defaults, options) ;
- 
-        this._defaults = defaults;
-        this._name = pluginName;
- 
-        this.init();
-    }
- 
-    Plugin.prototype.init = function () {
-        if(!this.hasTouchScreen()) {
-            $body.append($selectionSharing);
-            $twitterEl = $('');
-            $emailEl = $('');
-            // set binds
-            $('body').on('keypress keydown', _.debounce(updateSelection, 50)); 
-            $('body').on('mouseup', _.debounce(updateSelection, 200));
-            $('body').on('mousedown', _.debounce(updateSelection, 50));
-        }
+        this.isValidSelection = function(range){
+            return true;
+        };
+
     };
- 
-    // A really lightweight plugin wrapper around the constructor,
-    // preventing against multiple instantiations
-    $.fn[pluginName] = function ( options ) {
-        return this.each(function () {
-            if ( !$.data(this, "plugin_" + pluginName )) {
-                $.data( this, "plugin_" + pluginName,
-                new Plugin( this, options ));
-            }
-        });
-    }
- 
-})( jQuery, window, document );
+
+}));
+
