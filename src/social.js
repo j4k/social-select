0f451acea0f0a04a060cbdcb7a25fa2391bdf356
Jack Wall <jack.wall@thirtythree.co.uk>
2014-12-11 11:38:01 +0000

    going back to a jquery plugin

--- src/social.js
+++ src/social.js
@@ -1,30 +1,34 @@
-(function (factory) {
-    if (typeof define === 'function' && define.amd) { 
-        // AMD
-        define(['jquery', 'underscore'], factory);    
-    } else {
-        // No AMD.
-        factory(jQuery, _);
-    }
-}
-}(function ($, _) {
-   
-    // Defaults
-    var defaults = {
-        validAncestors: ['body'],
-        twitterTpl: "https://twitter.com/intent/tweet?text="{{text}}"&url={{url}}",
-        twitterMessageLimit: 140,
-        emailHrefTemplate: "mailto:?subject={{subject}}&body="{{selection}}" {{url}}'"
-    };
+;(function ( $, window, document, undefined ) {
+
+    // Create the defaults once
+    var pluginName = "socialSelect",
+        defaults = {
+          validAncestors: ['article', 'section'],
+          twitterTpl: "https://twitter.com/intent/tweet?text="{{text}}"&url={{url}}",
+          twitterMessageLimit: 140,
+          emailHrefTemplate: "mailto:?subject={{subject}}&body="{{selection}}" {{url}}'"
+        };
 
     // Set up vars
     var $body = $(document.body),
         $twitterEl,
         $emailEl;
 
-    $.fn.socialSelect = function(){
-        this.init = function(){
-            if(!this.hasTouchScreen()) {
+    function SocialSelect( element, options ) {
+        this.element = element;
+
+        this.options = $.extend( {}, defaults, options) ;
+
+        this._defaults = defaults;
+        this._name = pluginName;
+
+        this.init();
+    }
+
+    SocialSelect.prototype = {
+
+        init: function() {
+           if( !this.hasTouchScreen().bind(this) ) {
                 $body.append($selectionSharing);
                 $twitterEl = $('');
                 $emailEl = $('');
@@ -32,10 +36,10 @@
                 $('body').on('keypress keydown', _.debounce(this.updateSelection, 50)); 
                 $('body').on('mouseup', _.debounce(this.updateSelection, 200));
                 $('body').on('mousedown', _.debounce(this.updateSelection, 50));
-            }
-        };
-        
-        this.updateSelection = function(){
+           }
+        },
+
+        updateSelection: function(el, options) {
            var selection = window.getSelection && document.createRange && window.getSelection(),
                range,
                twitterMessage,
@@ -47,18 +51,33 @@
                twitterMessage = range.toString();
 
                if(!$.fn.socialSelect.isValidSelection(range) ){
-                // hideSelection()
-                    return;
+                  hideSelection()
+                  return;
                }
-
            }
-        };
+        },
 
-        this.isValidSelection = function(range){
+        isValidSelection = function(range){
             return true;
-        };
+        },
+
+        hideSelection = function(){
+            return true;
+        },
+
+        hasTouchScreen = function(){
+            return false;
+        }
 
     };
 
-}));
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName,
+                new SocialSelect( this, options ));
+            }
+        });
+    };
 
+})( jQuery, window, document );
